dee114673a6046220e7af3aacfb4d0b02bdaddd5
Ezequiel <ezequielamin@outlook.com>
2022-02-14 23:36:18 -0300

    agrego cors

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const morgan = require('morgan');
-const passport = require('passport');
-const session = require('express-session');
+const express = require("express");
+const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+const cors = require("cors");
 
 const app = express();
-const routesAuth = require('./routes/auth');
-const routesInfoAdmin = require('./routes/infoAdmin');
-const routesInfoUser = require('./routes/infoUser');
-const routesIndex = require('./routes/index');
-require('./database/database');
-require('./passport/auth-login');
+const routesAuth = require("./routes/auth");
+const routesInfoAdmin = require("./routes/infoAdmin");
+const routesInfoUser = require("./routes/infoUser");
+const routesIndex = require("./routes/index");
+require("./database/database");
+require("./passport/auth-login");
 
 //settings
-app.set('port', process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000);
 
 //middlewares
-app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: "https://rollingvet.netlify.app",
+    credentials: true,
+    allowedHeaders: true,
+  })
+);
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(session({
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
     secret: process.env.SECRET_KEY,
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -33,7 +42,6 @@ app.use(passport.session());
 app.use(routesAuth);
 app.use(routesInfoAdmin);
 app.use(routesInfoUser);
-app.use(routesIndex)
 
 app.listen(app.get("port"), () => {
   console.log(`Server on port ${app.get("port")}`);
