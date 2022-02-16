b2c1b156caf9e7a51531d7262c6eebe50eefef74
Ezequiel <ezequielamin@outlook.com>
2022-02-16 19:10:12 -0300

    prueba

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie : {
-      // sameSite: 'none',
+      sameSite: 'lax',
       // secure: true
       maxAge: 1000 * 60 * 60 * 24 * 7
     }
